44de08225682ecf61298ee5f91f0eb71a4d98924
relaxslow <relaxslow@hotmail.com>
2018-10-19 16:45:07 -0400

    u

--- client/views/content/webgl/drawLine/page.js
+++ client/views/content/webgl/drawLine/page.js
@@ -2,7 +2,7 @@
 xs.init = function (module) {
 
     module.selectDiv("drawLine")
-        .buildFrame(300, 300, "/client/views/content/webgl/drawline/line.html");
+        .buildFrame(300, 300, "/client/views/content/webgl/threeJsTest/iframe.html");
     module.selectDiv("lineCode")
         .load("/code", xs.COMPONENT, { text: "JS", code: "/threeJs/howToDrawLine" });
     module.selectDiv("dotCode")

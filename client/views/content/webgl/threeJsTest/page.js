449285ab45504cf43712bca27c316a945dd435db
relaxslow <relaxslow@hotmail.com>
2018-10-19 17:08:32 -0400

    u

--- client/views/content/webgl/threeJsTest/page.js
+++ client/views/content/webgl/threeJsTest/page.js
@@ -1,10 +1,9 @@
 xs.init = function (module) {
-    let wid = 400;
-    let hei = 300;
     module.selectDiv("threeJsTest")
     .buildFrame(400, 300, "/client/views/content/webgl/drawLine/displayFonts.html");
 
     module.selectDiv("threeJsBeginCode")
     .load("/code", xs.COMPONENT, { text: "JS", code: "/threeJs/howToDrawLine" });
-
+    module.selectDiv("testBox")
+    .buildFrame(400, 300, "/client/views/content/webgl/drawLine/line.html");
 }

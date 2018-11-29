11b3e29aa3fadd78a9be644a13afba03a8e6db9a
relaxslow <relaxslow@hotmail.com>
2018-11-29 10:48:47 -0500

    u

--- server/app.js
+++ server/app.js
@@ -452,7 +452,7 @@ let systemRoutine = {
 };
 let subRoutine = {
     "": function (req, res) {
-        // readHtmlFile("./client/index.html", res);
+        readHtmlFile("./client/index.html", res);
         // readHtmlFile("./iframes/begin/main.html", res);
         // readHtmlFile("./iframes/draw2D/main.html", res);
 
@@ -462,7 +462,7 @@ let subRoutine = {
 
 
         //vanilla js
-        readHtmlFile("./iframes/calendar/main.html", res);
+        // readHtmlFile("./iframes/calendar/main.html", res);
 
 
 
